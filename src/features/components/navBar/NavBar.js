730407561be9942cdcd3f9de20cf8163a4cbc2a1
Kaleb Lindevaldsen <kflindevaldsen@gmail.com>
2023-02-22 20:42:54 -0500

    NavBar Component CSS

---
+++ src/features/components/navBar/NavBar.js
@@ -0,0 +1,19 @@
+import './navBar.css'
+import { FaReddit } from 'react-icons/fa'
+import SearchBar from '../../slices/searchBar/SearchBar'
+
+const NavBar = () => {
+    return (
+        <header>
+            <div className='navBar'>
+                <div className='navBarLeft'>
+                    <FaReddit className='navBarIcon' />
+                    <p className='navBarTitle'>Reddit Clone</p>
+                </div>
+                <SearchBar />
+            </div>
+        </header>
+    )
+}
+
+export default NavBar
---
+++ src/features/components/posts/Posts.js
@@ -0,0 +1,7 @@
+const Posts = () => {
+    return (
+        <p>POSTS</p>
+    )
+}
+
+export default Posts
