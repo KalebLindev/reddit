f310e719603d6cec61f29486a2e34bbcf761c80c
Kaleb Lindevaldsen <kflindevaldsen@gmail.com>
2023-03-10 20:23:38 -0500

    Implemented fetching top 25 popular subreddits

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import subRedditsReducer from '../features/slices/subReddits/subRedditSlice';
 
 export const store = configureStore({
   reducer: {
+    allSubReddits: subRedditsReducer,
   },
 });
